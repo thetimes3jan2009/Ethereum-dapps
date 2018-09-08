8fb335e5c14f04653750bf638c4014b54ccad4e8
thetimes3jan2009 <42702091+thetimes3jan2009@users.noreply.github.com>
2018-09-08 16:43:36 -0700

    cleans up code by using modifiers

--- Escrow.sol
+++ Escrow.sol
@@ -4,6 +4,15 @@ contract Escrow{
     enum State{AWAITING_PAYMENT, AWAITING_DELIVERY, COMPLETE, REFUNDED}
     State public currentState = State.AWAITING_PAYMENT;// automatically initialized to first value for the enum
     
+    modifier buyerOnly(){
+        require(msg.sender==buyer || msg.sender==arbiter);
+        _;// the underscore is where the body of the modified function goes
+    }
+    
+    modifier sellerOnly(){
+        require(msg.sender==seller || msg.sender==arbiter);
+        _;
+    }
     address public buyer ;
     address public seller ;
     address public arbiter ;
@@ -14,22 +23,19 @@ contract Escrow{
         arbiter = _arbiter;
     }
     
-    function confirmPayment() payable{
-        require(msg.sender == buyer );
+    function confirmPayment() buyerOnly payable{
         require(currentState == State.AWAITING_PAYMENT);
         // note that this contract is a payable so it requires some payment to be sent to the contract which then becomes the balance of the contract but we haven't validated the value of the payment here??
         currentState = State.AWAITING_DELIVERY;
     }
     
-    function confirmDelivery(){
-        require(msg.sender == buyer || msg.sender == arbiter);
+    function confirmDelivery() buyerOnly {
         require(currentState == State.AWAITING_DELIVERY);
         seller.send(this.balance);
         currentState = State.COMPLETE;
     }
     
-    function refundBuyer(){
-        require(msg.sender == seller || msg.sender == arbiter);
+    function refundBuyer() sellerOnly{
         require(currentState == State.AWAITING_DELIVERY);
         buyer.send(this.balance);
         currentState = State.REFUNDED;
