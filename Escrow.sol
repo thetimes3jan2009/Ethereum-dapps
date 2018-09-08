51d6cd5101486f9f39accca66ea616025f01756d
thetimes3jan2009 <42702091+thetimes3jan2009@users.noreply.github.com>
2018-09-08 16:48:43 -0700

    adds parametrized modifiers to the functions

--- Escrow.sol
+++ Escrow.sol
@@ -13,6 +13,12 @@ contract Escrow{
         require(msg.sender==seller || msg.sender==arbiter);
         _;
     }
+    
+    modifier inState(State state){
+        require(currentState==state);
+        _;
+    }
+    
     address public buyer ;
     address public seller ;
     address public arbiter ;
@@ -23,20 +29,17 @@ contract Escrow{
         arbiter = _arbiter;
     }
     
-    function confirmPayment() buyerOnly payable{
-        require(currentState == State.AWAITING_PAYMENT);
+    function confirmPayment() buyerOnly inState(State.AWAITING_PAYMENT) payable{
         // note that this contract is a payable so it requires some payment to be sent to the contract which then becomes the balance of the contract but we haven't validated the value of the payment here??
         currentState = State.AWAITING_DELIVERY;
     }
     
-    function confirmDelivery() buyerOnly {
-        require(currentState == State.AWAITING_DELIVERY);
+    function confirmDelivery() buyerOnly inState(State.AWAITING_DELIVERY) {
         seller.send(this.balance);
         currentState = State.COMPLETE;
     }
     
-    function refundBuyer() sellerOnly{
-        require(currentState == State.AWAITING_DELIVERY);
+    function refundBuyer() sellerOnly inState(State.AWAITING_DELIVERY){
         buyer.send(this.balance);
         currentState = State.REFUNDED;
     }
